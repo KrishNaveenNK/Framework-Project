d66f017736c62483a427ff3ad458579183ecddea
KrishNaveenNK <krishnaveen021@gmail.com>
2022-11-06 23:40:16 +0530

    second time push

--- src/test/java/com/git/Git.java
+++ src/test/java/com/git/Git.java
@@ -2,4 +2,19 @@ package com.git;
 
 public class Git {
 
+	
+	private void Method1() {
+
+		System.out.println("java");
+	}
+	
+	private void Method2() {
+
+		System.out.println("Selenium");
+	}
+	
+	private void Method3() {
+
+		System.out.println("c++");
+	}
 }
